c7477e737f9f4196ba07df3eb5f16b6e5edd7e75
Valerii Oderii <swicher123@gmail.com>
2022-06-21 21:16:02 +0300

    Added layout and view

--- resources/views/partial/playoff.blade.php
+++ resources/views/partial/playoff.blade.php
@@ -1,5 +1,6 @@
-
-
-@php
-
-@endphp;
+@extends('layouts.default')
+@section('content')
+    @php
+        echo dd($tree);
+    @endphp
+@stop
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/playoff', [PlayoffController::class, 'build']);
+Route::get('/playoff', [PlayoffController::class, 'show']);
